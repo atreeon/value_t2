9b62e9f577f8ee7475628507f3c590e2865c5c8e
Atree <atreeonhill@gmail.com>
2020-03-25 08:11:49 +0000

    failing to do default values!

---
+++ example/test/ex21_test.dart
@@ -0,0 +1,21 @@
+import 'package:meta/meta.dart';
+import 'package:quiver_hashcode/hashcode.dart';
+import 'package:test/test.dart';
+import 'package:value_t2_annotation/value_t2_annotation.dart';
+
+part 'ex21_test.g.dart';
+
+//PRIVATE CONSTRUCTOR FOR DEFAULT VALUES
+
+//@ValueT2(privateConstructor: true)
+//abstract class $A {
+//  String a;
+//}
+
+//main() {
+//  test("1", () {
+//    var a = Pet(type: "cat");
+//
+//    expect(a.type, "cat");
+//  });
+//}
