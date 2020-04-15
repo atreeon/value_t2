fc4b4db450f3f6998ba1f47918a33fb1b6e7902a
Atree <atreeonhill@gmail.com>
2020-04-15 12:44:37 +0100

    ...

--- example/test/ex17x.dart
+++ example/test/ex17x.dart
@@ -1,6 +1,6 @@
 import 'package:value_t2_annotation/value_t2_annotation.dart';
 
-part 'ex17_.g.dart';
+part 'ex17x.g.dart';
 
 @ValueT2()
 abstract class $$Batch_Lesson {}
--- example/test/ex21_test.dart
+++ example/test/ex21_test.dart
@@ -1,8 +1,9 @@
 import 'package:meta/meta.dart';
 import 'package:quiver_hashcode/hashcode.dart';
+import 'package:test/test.dart';
 import 'package:value_t2_annotation/value_t2_annotation.dart';
 
-part 'ex21_.g.dart';
+part 'ex21_test.g.dart';
 
 //PRIVATE CONSTRUCTOR FOR DEFAULT VALUES USES UNDERSCORE AT END OF CLASS
 
@@ -11,6 +12,13 @@ abstract class $A_ {
   String a;
 }
 
+void main() {
+  test("1 default value", () {
+    var a = AFactory();
+    expect(a.a, "my default value");
+  });
+}
+
 A_ AFactory() {
-  return A_._(a: "blah");
+  return A_._(a: "my default value");
 }
